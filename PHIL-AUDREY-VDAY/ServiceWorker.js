efb4d84e4f16fc25d0e678e7ecaf637b852d862a
Phil Meyer III <philmeyer@Phils-MacBook-Air.local>
2025-04-08 12:13:23 -0700

    adding phil-audrey

---
+++ PHIL-AUDREY-VDAY/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-V-Day-1.0";
+const contentToCache = [
+    "Build/PHIL-AUDREY-VDAY.loader.js",
+    "Build/PHIL-AUDREY-VDAY.framework.js.br",
+    "Build/PHIL-AUDREY-VDAY.data.br",
+    "Build/PHIL-AUDREY-VDAY.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
