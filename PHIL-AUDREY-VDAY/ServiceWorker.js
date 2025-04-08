9f98c5de75d59571261ff6eb99c9d862895bb595
Phil Meyer III <philmeyer@Phils-MacBook-Air.local>
2025-04-08 13:26:54 -0700

    revert back to uncomp

--- PHIL-AUDREY-VDAY/ServiceWorker.js
+++ PHIL-AUDREY-VDAY/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-V-Day-1.0";
 const contentToCache = [
     "Build/PHIL-AUDREY-VDAY.loader.js",
-    "Build/PHIL-AUDREY-VDAY.framework.js.gz",
-    "Build/PHIL-AUDREY-VDAY.data.gz",
-    "Build/PHIL-AUDREY-VDAY.wasm.gz",
+    "Build/PHIL-AUDREY-VDAY.framework.js",
+    "Build/PHIL-AUDREY-VDAY.data",
+    "Build/PHIL-AUDREY-VDAY.wasm",
     "TemplateData/style.css"
 
 ];
